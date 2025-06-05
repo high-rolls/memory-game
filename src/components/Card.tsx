d9ce213bbfb0ebd8cf9781b19752b055361df810
Paulo Vitor F. G. Pivotto <paulo_vitor652@hotmail.com>
2025-06-05 15:03:55 -0300

    Fix responsiveness on medium screens (my work laptop)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ type CardProps = CardData & {
 const Card = ({ value, flipped, matched, onClick }: CardProps) => {
   return (
     <button
-      className={`w-[64px] h-[64px] sm:w-[128px] sm:h-[128px] flex justify-center items-center 
+      className={`w-[64px] h-[64px] sm:w-[96px] sm:h-[96px] 2xl:w-[128px] 2xl:h-[128px] flex justify-center items-center 
         rounded-md text-5xl sm:text-8xl ${
           flipped ? "bg-amber-100" : matched ? "bg-lime-300" : "bg-amber-600"
         }`}
