7ad2a6fe1c216dc9b4f17f7559781e42e358dc6e
Paulo Vitor F. G. Pivotto <paulo_vitor652@hotmail.com>
2025-06-16 02:08:17 -0300

    Add top navbar

--- src/routes/memory-game-layout.tsx
+++ src/routes/memory-game-layout.tsx
@@ -1,20 +1,37 @@
 // src/routes/MemoryGameLayout.tsx
-import { NavLink, Outlet } from "react-router";
+import { Link, NavLink, Outlet } from "react-router";
 import { GameSettingsProvider } from "@/context/game-settings";
 import { GamepadIcon, SettingsIcon } from "lucide-react";
 
 export default function MemoryGameLayout() {
   return (
     <GameSettingsProvider>
-      <div className="fixed top-0 right-0 bottom-16 left-0">
+      <div className="hidden md:navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <Link to="/play" className="btn btn-ghost text-xl">
+            memoji
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link to="/">
+                <SettingsIcon size={16} /> Settings
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="fixed top-0 md:top-16 right-0 bottom-12 md:bottom-0 left-0">
         <Outlet />
       </div>
-      <div className="dock">
+      <div className="dock dock-xs md:hidden">
         <NavLink
           to="/play"
           className={({ isActive }) => (isActive ? "dock-active" : undefined)}
         >
           <GamepadIcon size={20} />
+          <span className="dock-label">Game</span>
         </NavLink>
 
         <NavLink
@@ -22,6 +39,7 @@ export default function MemoryGameLayout() {
           className={({ isActive }) => (isActive ? "dock-active" : undefined)}
         >
           <SettingsIcon size={20} />
+          <span className="dock-label">Settings</span>
         </NavLink>
       </div>
     </GameSettingsProvider>
